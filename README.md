eec2d601cac9e07a0b57a1c59a00a1e95380fed2
Luca Happel <lucahappel99@gmx.de>
2019-10-20 13:15:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+A library aiming to implement at least some of the features found in
+[https://www.tutorialspoint.com/nodejs/nodejs_path_module.htm](https://www.tutorialspoint.com/nodejs/nodejs_path_module.htm)
+for use as a module in your browser
+
+| Function | Supported |
+|----------|-----------|
+| normalize | ✅ |
+| join | ✅ |
+| resolve | ❌ |
+| isAbsolute | ✅ |
+| relative | ❌ |
+| dirname | ❌ |
+| basename | ❌ |
+| extname | ❌ |
---
+++ path.mjs
@@ -0,0 +1,49 @@
+// FILE:	path.mjs
+// AUTHOR:	Luca Leon Happel
+// DATE:	20.10.2019
+// LINK:	https://www.tutorialspoint.com/nodejs/nodejs_path_module.htm
+
+// URL -> URL
+const normalize = u => {
+	let p = u.split('/');
+	return p
+		.filter((e,i) => p[i+1]!='..')
+		.filter(e => e!='..' && e!='.' && e!='')
+		.join('/')
+}
+
+// [URL] -> URL
+const join = (...u) => normalize(u.join('/'))
+
+// URL -> Boolean
+const isAbsolute = u => !u
+	.split('/')
+	.some(e => e == '..')
+
+// URL -> Url
+const dirname = u => u[u.length-1] != '/'
+	? u
+		.split('/')
+		.slice(0,-1)
+		.join('/')
+		.concat('/')
+	: u
+
+const basename = u => u[u.length-1] != '/'
+	? u
+		.split('/')
+		.pop()
+	: ''
+
+const extname = u => basename(u)
+	.split('.')
+	.pop()
+
+export {
+	normalize,
+	join,
+	isAbsolute,
+	dirname,
+	basename,
+	extname
+}
